624aa32618b00301a6bc3d6b533151815f478c2b
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-12-23 06:26:10 +0900

    loadイベントで画像読み込み完了時に画像表示する処理記述

--- dog/dog.js
+++ dog/dog.js
@@ -1,7 +1,6 @@
 'use strict'
 
 // ①XMLHttpRequestを使用した書き方
-
 // ◆手順
 // 1 要素の取得→変数へ格納（ボタン、画像、APIのURL）
 // 2 初期状態を非表示
@@ -34,15 +33,14 @@ dogBtnElm.addEventListener('click', function () {
 });
 
 // ②Fetchを使用した書き方
-
 // ◆手順
 // 1 要素の取得→変数へ格納（ボタン、画像、APIのURL）
 // 2 初期状態を非表示
 // 3 ボタン要素のclickイベント作成（ボタンクリック後、API画像が表示される処理）
-// 4 fetchメソッドでAPIを読込
-// 5 JSONを呼び出す
-// 6 画像要素の属性にJSONのURL（message）を代入
-// 7 画像表示
+// 4 loadイベントで画像読み込み完了時に画像表示する処理記述
+// 5 fetchメソッドでAPIを読込
+// 6 JSONを呼び出す
+// 7 画像要素の属性にJSONのURL（message）を代入
 
 let dogBtnElm2 = document.getElementById('dog-btn2');
 let imgElm2 = document.getElementById('image2');
@@ -51,6 +49,14 @@ imgElm2.style.display = 'none';
 
 dogBtnElm2.addEventListener('click', function () { 
   
+  // 画像読み込み完了したときの処理
+  imgElm2.addEventListener('load', displayImage)
+  
+  function displayImage() {
+    // CSSを変更する処理
+    imgElm2.style.display = '';
+  };
+  
   fetch(imgApi2)
   .then(response => {
     return response.json();
@@ -58,20 +64,18 @@ dogBtnElm2.addEventListener('click', function () {
   .then(apiData2 => {
     imgElm2.src = apiData2.message;
   })
-  imgElm2.style.display = '';
-
+  
 });
   
   // ③axiosを使用した書き方
-  
   // ◆手順
   // 1 要素の取得→変数へ格納（ボタン、画像、APIのURL）
   // 2 初期状態を非表示
   // 3 ボタン要素のclickイベント作成（ボタンクリック後、API画像が表示される処理）
-  // 4 axios.getメソッドでAPIを読込
-  // 5 response.dataでJSONのデータを取得
-  // 6 画像要素の属性にJSONのURL（message）を代入
-  // 7 画像表示
+  // 4 loadイベントで画像読み込み完了時に画像表示する処理記述
+  // 5 axios.getメソッドでAPIを読込
+  // 6 response.dataでJSONのデータを取得
+  // 7 画像要素の属性にJSONのURL（message）を代入
   
 let dogBtnElm3 = document.getElementById('dog-btn3');
 let imgElm3 = document.getElementById('image3');
@@ -79,10 +83,22 @@ let imgApi3 = 'https://dog.ceo/api/breeds/image/random';
 imgElm3.style.display = 'none';
 
 dogBtnElm3.addEventListener('click', function () {
-
+  
+  // 画像読み込み完了したときの処理①
+  imgElm3.addEventListener('load', displayImage)
+  
+  function displayImage() {
+    // CSSを変更する処理
+    imgElm3.style.display = '';
+  };
+  
+  // 画像読み込み完了時に画像表示する処理②
+  // imgElm3.addEventListener('load', function () {
+  //     imgElm3.style.display = '';
+  // });
+  
   axios.get(imgApi3)
   .then(response => response.data)
   .then(apiData3 => imgElm3.src = apiData3.message)
-  imgElm3.style.display = '';
-
+  
 });
