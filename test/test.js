cdca66cf645fd176bcfff92070e9e61a46cc7f11
n-haraguchi <yasaiseikatu0306@gmail.com>
2022-01-29 06:57:10 +0900

    東京天気データコンソール取得

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// 復習1：要素を取得、変更
+// 要素の取得
+
+
+
+
+function roopTest() {
+  for (let i = 1; i < 100; i++) {
+    console.log(roopTest());
+  }
+
+};
--- weather/weather.js
+++ weather/weather.js
@@ -1,2 +1,43 @@
 'use strict'
 
+ // ③axiosを使用した書き方
+  // ◆手順
+  // 1 要素の取得→変数へ格納（ボタン、画像、APIのURL）
+  // 2 初期状態を非表示
+  // 3 ボタン要素のclickイベント作成（ボタンクリック後、API画像が表示される処理）
+  // 4 loadイベントで画像読み込み完了時に画像表示する処理記述
+  // 5 axios.getメソッドでAPIを読込
+  // 6 response.dataでJSONのデータを取得
+  // 7 画像要素の属性にJSONのURL（message）を代入
+
+//   let weatherElm = document.getElementById('weather-btn');
+//   // let imgElm = document.getElementById('img-box');
+//   let apiElm = 'https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m';
+
+
+//   // weatherElm.addEventListener('click', function () {
+
+//   //   axios.get(apiElm)
+//   //   .then(response => response.data)
+//   //   .then(apiData => imgElm.src = apiData.message)
+
+//   // });
+
+// weatherElm.addEventListener('click', function () {
+  
+//     // JSON
+//     // const option = {responseType: "blob"};
+//     axios.get(apiElm)
+//     .then(response => {response.data.text()
+//     .then(apiData => {
+//         let arr = JSON.parse(apiData);
+//         imgElm.src
+//     });
+  
+//   });
+
+
+let apiElm = 'https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m';
+
+	axios.get(apiElm)
+	.then(response => console.log(response.data));
